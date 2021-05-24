2c12e0d56be18cdea09f4fa2bd705a569732c2ad
joacofum <joacofum@gmail.com>
2021-05-24 16:54:09 -0300

    Listar comprobantes ordenado y bien hecho, cotizaciones casi hecho.

--- src/GUI/altaArticulo.java
+++ src/GUI/altaArticulo.java
@@ -122,10 +122,6 @@ public class altaArticulo extends javax.swing.JFrame {
         
     }
 
-    altaArticulo(Cotizacion c, JTable jTable1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
